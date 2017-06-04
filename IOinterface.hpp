c1830dd55ee9b055761cd149118793654feea565
noedelima <noedelima@gmail.com>
2017-06-03 21:29:02 -0300

    Add files via upload

---
+++ IOinterface.hpp
@@ -0,0 +1,16 @@
+/*
+ * IOinterface.h
+ *
+ *  Created on: 3 de jun de 2017
+ *      Author: noedelima
+ */
+
+#ifndef IOINTERFACE_HPP_
+#define IOINTERFACE_HPP_
+
+#include "sales.hpp"
+queue<saling> getdata();
+
+
+
+#endif /* IOINTERFACE_HPP_ */
